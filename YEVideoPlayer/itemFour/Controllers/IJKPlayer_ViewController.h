cbc09a57e99da516e12c421025c27475e59810a3
yongen <280798744@qq.com>
2017-05-26 14:54:30 +0800

    IJKPlayer播放器的使用，在这里由于frame库过大未上传，自己手动导入即可

---
+++ YEVideoPlayer/itemFour/Controllers/IJKPlayer_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  IJKPlayer_ViewController.h
+//  YEVideoPlayer
+//
+//  Created by yongen on 2017/5/26.
+//  Copyright © 2017年 yongen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJKPlayer_ViewController : UIViewController
+
+@end
